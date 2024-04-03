3d44ad4b38bbeff85f99182a7123bf1764c6174f
Ethan <ethansleightholm@hotmail.com>
2024-04-02 19:19:45 -0600

    Icons/Buttons/APK
    Added new icons, better buttons on home screen, and built an APK

--- app/index.js
+++ app/index.js
@@ -1,28 +1,38 @@
-import { Text, View, ImageBackground } from "react-native";
+import { Text, View, ImageBackground, Pressable } from "react-native";
 import { Link } from "expo-router";
 import styles from "../Styles/Stylesheet";
 import BackgroundImage from "../assets/background.png";
 
 export default function App() {
   return (
-      <ImageBackground source={BackgroundImage} style={styles.backgroundImage}>
-        <View style={styles.content}>
+    <ImageBackground source={BackgroundImage} style={styles.backgroundImage}>
+      <View style={styles.content}>
         <Text style={styles.title}>SOUNDBOARD</Text>
-          <View style={styles.button}>
-            <Link href={"/premade"}>
-              <View>
-                <Text style={styles.buttonText}>Premade</Text>
-              </View>
-            </Link>
-          </View>
-          <View style={styles.button}>
-            <Link href={"/custom"}>
-              <View>
-                <Text style={styles.buttonText}>Custom</Text>
-              </View>
-            </Link>
-          </View>
-        </View>
-      </ImageBackground>
+        <Link
+          style={({ pressed }) => [
+            styles.button,
+            pressed ? styles.buttonRecording : {}, // Dynamically changes style when pressed
+          ]}
+          href={{ pathname: "/premade" }}
+          asChild
+        >
+          <Pressable>
+            <Text style={styles.buttonText}>Premade</Text>
+          </Pressable>
+        </Link>
+        <Link
+          style={({ pressed }) => [
+            styles.button,
+            pressed ? styles.buttonRecording : {}, // Dynamically changes style when pressed
+          ]}
+          href={{ pathname: "/custom" }}
+          asChild
+        >
+          <Pressable>
+            <Text style={styles.buttonText}>Custom</Text>
+          </Pressable>
+        </Link>
+      </View>
+    </ImageBackground>
   );
 }
